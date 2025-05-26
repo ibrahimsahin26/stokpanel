db3b2fe7981283abe655e221f8f667b4c1731585
ibrahimsahin26 <ibrahim@hepcazip.com>
2025-05-26 19:04:52 +0300

    Update ofis26_fiyat_guncelle.py

--- pages/ofis26_fiyat_guncelle.py
+++ pages/ofis26_fiyat_guncelle.py
@@ -1,67 +1,79 @@
-# pages/ticimax_fiyat_cek.py
 import streamlit as st
 import pandas as pd
-from zeep import Client
-import os
+import requests
+from bs4 import BeautifulSoup
 
-st.title("Ticimax Fiyat Güncelleme")
+st.title("Ticimax Ofis26 Satış Fiyatı Güncelle")
 
-# CSV yolu
+# Ana ürün listesini yükle
 csv_path = "pages/veri_kaynaklari/ana_urun_listesi.csv"
+df = pd.read_csv(csv_path)
 
-# SOAP bağlantısı
-wsdl_url = "https://ws.ticimax.com/Service.asmx?WSDL"
-client = Client(wsdl=wsdl_url)
+# Butonla işlemi tetikle
+if st.button("Ticimax Ofis26 Satış Fiyatlarını Güncelle"):
+    st.info("Ticimax API'den satış fiyatları çekiliyor...")
 
-# Yetki kodu (Ticimax tarafından verilir)
-uye_kodu = "HVEKN1KK1USEAD0VAXTVKP8FWGN3AE"
+    # SOAP API ayarları
+    url = "https://ofis26.ticimax.com/servis/servis.asmx"
+    headers = {"Content-Type": "text/xml; charset=utf-8", "SOAPAction": "http://tempuri.org/SelectUrun"}
 
-# SOAP parametreleri
-filtre = {
-    "Aktif": -1,
-    "Firsat": -1,
-    "Indirimli": -1,
-    "Vitrin": -1,
-    "KategoriID": 0,
-    "MarkaID": 0,
-    "TedarikciID": -1,
-    "ToplamStokAdediBas": 0,
-    "ToplamStokAdediSon": 100000
-}
-sirala = {
-    "BaslangicIndex": 0,
-    "KayitSayisi": 1000,
-    "SiralamaDegeri": "ID",
-    "SiralamaYonu": "DESC"
-}
+    uye_kodu = "HVEKN1KK1USEAD0VAXTVKP8FWGN3AE"
+    baslangic_index = 0
+    kayit_sayisi = 100
+    fiyatlar = {}
 
-# Ürünleri çek
-try:
-    response = client.service.SelectUrun(UyeKodu=uye_kodu, f=filtre, s=sirala)
-    if not response:
-        st.error("Ticimax'ten yanıt alınamadı.")
-    else:
-        ticimax_urunler = response.SelectUrunResult.TicimaxUrunDetaylari.TicimaxUrunDetay
-        data = []
-        for urun in ticimax_urunler:
-            data.append({
-                "StokKodu": urun.StokKodu,
-                "SatisFiyati": urun.SatisFiyati
-            })
-        fiyat_df = pd.DataFrame(data)
+    while True:
+        # SOAP XML talebi
+        payload = f"""<?xml version="1.0" encoding="utf-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/" xmlns:ns="http://schemas.datacontract.org/2004/07/">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <tem:SelectUrun>
+         <tem:UyeKodu>{uye_kodu}</tem:UyeKodu>
+         <tem:f>
+            <ns:Aktif>-1</ns:Aktif>
+            <ns:Firsat>-1</ns:Firsat>
+            <ns:Indirimli>-1</ns:Indirimli>
+            <ns:Vitrin>-1</ns:Vitrin>
+            <ns:KategoriID>0</ns:KategoriID>
+            <ns:MarkaID>0</ns:MarkaID>
+            <ns:TedarikciID>-1</ns:TedarikciID>
+            <ns:ToplamStokAdediBas>0</ns:ToplamStokAdediBas>
+            <ns:ToplamStokAdediSon>100000</ns:ToplamStokAdediSon>
+            <ns:UrunKartiID>0</ns:UrunKartiID>
+         </tem:f>
+         <tem:s>
+            <ns:BaslangicIndex>{baslangic_index}</ns:BaslangicIndex>
+            <ns:KayitSayisi>{kayit_sayisi}</ns:KayitSayisi>
+            <ns:SiralamaDegeri>ID</ns:SiralamaDegeri>
+            <ns:SiralamaYonu>DESC</ns:SiralamaYonu>
+         </tem:s>
+      </tem:SelectUrun>
+   </soapenv:Body>
+</soapenv:Envelope>"""
 
-        # Ana tabloyu oku
-        ana_df = pd.read_csv(csv_path)
+        response = requests.post(url, data=payload, headers=headers)
+        soup = BeautifulSoup(response.content, 'xml')
+        urunler = soup.find_all("Urun")
 
-        # Satış fiyatlarını eşleştir
-        ana_df = ana_df.merge(fiyat_df, how="left", left_on="Stok Kodu", right_on="StokKodu")
-        ana_df["Ofis26 Satış Fiyatı"] = ana_df["SatisFiyati"]
-        ana_df.drop(columns=["StokKodu", "SatisFiyati"], inplace=True)
+        if not urunler:
+            break  # Daha fazla kayıt yok
 
-        # Güncellenmiş tabloyu kaydet
-        ana_df.to_csv(csv_path, index=False)
-        st.success("Ofis26 Satış Fiyatı başarıyla güncellendi!")
-        st.dataframe(ana_df)
+        for urun in urunler:
+            stok_kodu = urun.find("StokKodu")
+            satis_fiyati = urun.find("SatisFiyati")
+            if stok_kodu and satis_fiyati:
+                fiyatlar[stok_kodu.text.strip()] = float(satis_fiyati.text.strip())
 
-except Exception as e:
-    st.error(f"Hata oluştu: {e}")
+        baslangic_index += kayit_sayisi
+
+    # Ana tabloya satış fiyatlarını yaz
+    df["Ofis26 Satış Fiyatı"] = df["Stok Kodu"].apply(lambda x: fiyatlar.get(str(x), ""))
+
+    # Güncel tabloyu göster
+    st.success("Satış fiyatları güncellendi.")
+    st.dataframe(df)
+
+    # Güncel CSV'ye yaz
+    df.to_csv(csv_path, index=False)
+    st.info("CSV dosyasına yazıldı.")
