0b2987ab0cc56cb87ef2c6eca1841b56460fec1f
ibrahimsahin26 <ibrahim@hepcazip.com>
2025-05-26 23:41:37 +0300

    Update ana_urun_listesi_butonlu.py

--- pages/ana_urun_listesi_butonlu.py
+++ pages/ana_urun_listesi_butonlu.py
@@ -1,5 +1,7 @@
-import streamlit as st
-import pandas as pd
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from veri_kaynaklari.mikro_api_panel import mikro_guncelle
 st.title("📦 HEpcazip Mikro API Entegrasyonu")
 
